4d93f16fb71417b522839468bcacf47940289afa
Nick <ntrogh@hotmail.com>
2017-04-11 21:54:28 +0200

    Publish solution challenge 3

---
+++ Challenge 3/src/XamarinAlliance/XamarinAllianceApp/Services/CharacterService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using XamarinAllianceApp.Helpers;
+using XamarinAllianceApp.Models;
+
+namespace XamarinAllianceApp.Services
+{
+    public class CharacterService
+    {
+        private MobileServiceClient Client;
+        private IMobileServiceTable<Character> CharacterTable;
+
+        public CharacterService()
+        {
+            Client = new MobileServiceClient(Constants.MobileServiceClientUrl);
+            CharacterTable = Client.GetTable<Character>();
+        }
+
+        /// <summary>
+        /// Get the list of characters
+        /// </summary>
+        /// <returns>ObservableCollection of Character objects</returns>
+        public async Task<ObservableCollection<Character>> GetCharactersAsync()
+        {
+            try
+            {
+                var query = CharacterTable.OrderBy(c => c.Name);
+                var characters = await query.ToListAsync();
+
+                return new ObservableCollection<Character>(characters);
+            }
+            catch (Exception)
+            {                
+                throw;
+            }
+        }
+    }
+}
