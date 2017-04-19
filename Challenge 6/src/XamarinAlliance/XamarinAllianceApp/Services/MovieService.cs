deea1a4cd76e31ef68e0e484a86b78ceef49e464
Depechie <glenn.versweyveld@gmail.com>
2017-04-19 20:54:53 +0200

    Initial commit challenge 6

---
+++ Challenge 6/src/XamarinAlliance/XamarinAllianceApp/Services/MovieService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+using XamarinAllianceApp.Helpers;
+using XamarinAllianceApp.Models;
+
+namespace XamarinAllianceApp.Services
+{
+    public class MovieService
+    {
+        private MobileServiceClient Client;
+        private IMobileServiceTable<Movie> MovieTable;
+
+        public MovieService()
+        {
+        	Client = new MobileServiceClient(Constants.MobileServiceClientUrl);
+        	MovieTable = Client.GetTable<Movie>();
+        }
+
+        /// <summary>
+        /// Get the list of movies
+        /// </summary>
+        /// <returns>ObservableCollection of Character objects</returns>
+        public async Task<ObservableCollection<Movie>> GetMoviesAsync()
+        {
+        	try
+        	{
+                var query = MovieTable.OrderBy(c => c.Title);
+        		var movies = await query.ToListAsync();
+
+                return new ObservableCollection<Movie>(movies);
+        	}
+        	catch (Exception)
+        	{
+        		throw;
+        	}
+        }
+    }
+}
